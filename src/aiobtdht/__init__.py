73f28d399802f9476a445af53a82987d132af8d9
D.A.Bashkirtsev <bashkirtsevich@gmail.com>
2019-08-09 00:48:57 +0500

    Increase version

--- src/aiobtdht/__init__.py
+++ src/aiobtdht/__init__.py
@@ -1,7 +1,7 @@
 from aiobtdht.dht import DHT
 from distutils.version import LooseVersion
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __version_info__ = tuple(LooseVersion(__version__).version)
 __all__ = [
     "DHT"
